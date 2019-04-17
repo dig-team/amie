afa5d30cb22a272baec39e9c4e2cdd57d6e741f3
Jonathan Lajus <jlajus@enst.fr>
2019-04-17 15:14:04 +0200

    [labelMining] predicate label

--- kb/src/main/java/amie/data/MultilingualKB.java
+++ kb/src/main/java/amie/data/MultilingualKB.java
@@ -17,8 +17,8 @@ public class MultilingualKB extends KB {
     protected boolean add(ByteString subject, ByteString relation, ByteString object) {
         String[] split = object.toString().split("@");
         if (split.length == 2) {
-            super.add(object, ByteString.of("<label@>"), ByteString.of(split[0]));
-            super.add(object, ByteString.of("<@lang>"), ByteString.of(split[1]));
+            super.add(object, ByteString.of("<label>"), ByteString.of(split[0]));
+            super.add(object, ByteString.of("<lang>"), ByteString.of(split[1]));
         }
         return super.add(subject, relation, object);
     }

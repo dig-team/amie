b9085ab5a15dbb3314c82a7349c6b8e4d8f4e06c
Antonio Carlos <falcaopetri@gmail.com>
2020-06-08 09:52:26 -0300

    [AMIEParser] Read TSV support

--- rules/src/main/java/amie/rules/AMIEParser.java
+++ rules/src/main/java/amie/rules/AMIEParser.java
@@ -14,6 +14,7 @@ import it.unimi.dsi.fastutil.ints.IntList;
 
 import javatools.datatypes.Pair;
 import javatools.filehandlers.FileLines;
+import javatools.filehandlers.TSVFile;
 
 /**
  * Parses a file of AMIE rules
@@ -55,28 +56,13 @@ public class AMIEParser {
 
     public static List<Rule> rules(File f) throws IOException {
         List<Rule> result = new ArrayList<>();
-        for (String line : new FileLines(f)) {
-            ArrayList<int[]> triples = KB.triples(line);
-            if (triples == null || triples.size() < 2) continue;
-            int[] last = triples.get(triples.size() - 1);
-            triples.remove(triples.size() - 1);
-            triples.add(0, last);
-            Rule query = new Rule();
+        for (List<String> record : new TSVFile(f)) {
+            Rule query = rule(record.get(0));
 
-            IntList variables = new IntArrayList();
-            for (int[] triple : triples) {
-                if (!variables.contains(triple[0]))
-                    variables.add(triple[0]);
-                if (!variables.contains(triple[2]))
-                    variables.add(triple[2]);
-            }
-
-            query.setSupport(0);
-            query.setTriples(triples);
-            query.setFunctionalVariablePosition(0);
-            result.add(query);
+            if (query != null)
+                result.add(query);
         }
-        return (result);
+        return result;
     }
 
     public static void main(String[] args) throws Exception {

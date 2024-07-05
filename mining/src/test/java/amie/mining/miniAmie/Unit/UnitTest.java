24cb0124a21d5127283e144cec7d486a43baf71e
vguerbet <valentin.guerbette@inria.fr>
2024-07-05 17:55:33 +0200

    - (broken) Implemented utils functions

---
+++ mining/src/test/java/amie/mining/miniAmie/Unit/UnitTest.java
@@ -0,0 +1,12 @@
+package amie.mining.miniAmie.Unit;
+
+import amie.data.AbstractKB;
+import junit.framework.TestCase;
+
+public abstract class UnitTest extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp() ;
+
+    }
+}

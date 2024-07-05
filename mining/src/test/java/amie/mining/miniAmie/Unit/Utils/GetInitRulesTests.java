d346e73983b7d65e28af24f3e7c26e986d76f5f4
vguerbet <valentin.guerbette@inria.fr>
2024-07-05 17:55:33 +0200

    - (broken) Implemented utils functions

---
+++ mining/src/test/java/amie/mining/miniAmie/Unit/Utils/GetInitRulesTests.java
@@ -0,0 +1,18 @@
+package amie.mining.miniAmie.Unit.Utils;
+
+import amie.mining.miniAmie.Unit.UnitTest;
+
+public class GetInitRulesTests extends UnitTest {
+
+    // TODO
+
+    /**
+     * Checks that single atom rules produced by GetInitRules are above min support
+     */
+    public void testMinSupport() {
+        assert true ;
+    }
+
+    // TODO more tests
+
+}
